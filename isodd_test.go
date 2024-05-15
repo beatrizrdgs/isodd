f4f9cdce0a75ddb1f02d31d16c790ef3a1ed5f8b
Beatriz Rodrigues <beatrizrdgs@users.noreply.github.com>
2024-05-15 16:12:51 -0300

    refactor(test): use args int

--- isodd_test.go
+++ isodd_test.go
@@ -3,28 +3,25 @@ package isodd
 import "testing"
 
 func TestIsOdd(t *testing.T) {
-	type args struct {
-		n int
-	}
 	tests := []struct {
 		name string
-		args args
+		args int
 		want bool
 	}{
-		{"1", args{1}, true},
-		{"2", args{2}, false},
-		{"3", args{3}, true},
-		{"4", args{4}, false},
-		{"5", args{5}, true},
-		{"6", args{6}, false},
-		{"7", args{7}, true},
-		{"8", args{8}, false},
-		{"9", args{9}, true},
-		{"0", args{10}, false},
+		{"0", 0, false},
+		{"1", 1, true},
+		{"2", 2, false},
+		{"3", 3, true},
+		{"4", 4, false},
+		{"5", 5, true},
+		{"6", 6, false},
+		{"7", 7, true},
+		{"8", 8, false},
+		{"9", 9, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := IsOdd(tt.args.n); got != tt.want {
+			if got := IsOdd(tt.args); got != tt.want {
 				t.Errorf("IsOdd() = %v, want %v", got, tt.want)
 			}
 		})
